2fa81db851ddb9f00a0d32f6876b21ce67d36c04
Mike Kelly <kellysystemsrepair@gmail.com>
2020-10-13 23:23:13 -0400

    sorted env vars, still having webpacker issues

--- init.sql
+++ init.sql
@@ -1,1 +1,3 @@
-CREATE USER boats_development SUPERUSER;
+CREATE USER boats_development;
+CREATE DATABASE boats_development;
+GRANT ALL PRIVILEGES ON DATABASE boats_development TO boats_development;
